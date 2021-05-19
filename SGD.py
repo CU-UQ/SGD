83350b601bd427414155bea32f7f8bca36a759fd
subhayande <40906424+subhayande@users.noreply.github.com>
2021-05-19 00:50:22 -0600

    Update SGD.py

--- SGD.py
+++ SGD.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
+""" 
+-------------------------------------------------------------------------------
+If you find this code useful please cite the article:
+
+Topology Optimization under Uncertainty using a Stochastic Gradient-based Approach
+Subhayan De, Jerrad Hampton, Kurt Maute, and Alireza Doostan (2020)
+Structural and Multidisciplinary Optimization, 62(5), 2255-2278. https://doi.org/10.1007/s00158-020-02599-z
+
+BibTeX entry:
+@article{de2020topology,
+title={Topology optimization under uncertainty using a stochastic gradient-based approach},
+author={De, Subhayan and Hampton, Jerrad and Maute, Kurt and Doostan, Alireza},
+journal={Structural and Multidisciplinary Optimization},
+volume={62},
+number={5},
+pages={2255--2278},
+year={2020},
+publisher={Springer}
+}
+
+Download the SGD module from https://github.com/CU-UQ/SGD.
+See the demo https://github.com/CU-UQ/SGD/blob/master/sgd_demo.py for an example of the implementation.
+For a description of the algorithms, see De et al (2020) (https://doi.org/10.1007/s00158-020-02599-z) and Ruder (2016) (https://arxiv.org/abs/1609.04747).
+-------------------------------------------------------------------------------
+
 This is the class file that implements: 
 (i) Stochastic Gradient Descent, 
 (ii) SGD with Momentum,
